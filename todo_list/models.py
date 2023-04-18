2e5428e16152692ffd3bdfd6b49d7acba61fcb69
devHksingh <hksingh.dev@gmail.com>
2023-04-18 20:31:59 +0530

    initailize database and migrate and update database admin

--- todo_list/models.py
+++ todo_list/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class List(models.Model):
     item = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-
+    # first_name =
+    # Last_name = 
+    # email = 
     def __str__(self):
-        return self.item
+        return self.item +' | ' +str(self.completed)
