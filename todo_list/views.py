5a0596cbce91546e27d4f4b87ae355308efcdaf4
devHksingh <hksingh.dev@gmail.com>
2023-04-20 15:36:04 +0530

    add form.py, update views.py base.html admin.py

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from .models import List
+from .forms import ListForm
 
 # Create your views here.
 
 def home(request):
-    all_items = List.objects.all
-    return render(request,'home.html',{'all_items':all_items})
+
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            return render(request,'home.html',{'all_items':all_items})
+    
+    else:
+        all_items = List.objects.all
+        return render(request,'home.html',{'all_items':all_items})
 
 def about(request):
     my_name ="Harshit Kumar Singh"
