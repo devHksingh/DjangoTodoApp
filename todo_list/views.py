1dfef51509b9f6ff059f39d7b676264c73db4dc2
devHksingh <hksingh.dev@gmail.com>
2023-04-24 21:24:56 +0530

    update home view , added alert messages and activate delete items btn

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -12,6 +14,7 @@ def home(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
+            messages.success(request,('Item Has Been Added To List!'))
             return render(request,'home.html',{'all_items':all_items})
     
     else:
@@ -21,3 +24,9 @@ def home(request):
 def about(request):
     my_name ="Harshit Kumar Singh"
     return render(request,'about.html',{'name':my_name})
+
+def delete(request,list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request,('Item Has Been Deleted!'))
+    return redirect('home')
